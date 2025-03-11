eca7d6512408a42b7b72f66a571d66148ff23b35
Maike Basmer <maike.basmer@hu-berlin.de>
2025-03-11 20:30:54 +0100

    separated view generation from eval

--- src/evaluation/leading_type_eval.py
+++ src/evaluation/leading_type_eval.py
@@ -1,40 +1,9 @@
-from networkx.classes import edges
-from ocpa.objects.log.importer.csv import factory as ocel_import_factory
 from src.strategies.subset_selection import SubsetSelector
-
+from src.view_generation.leading_type import compute_edges_by_leading_type
 
 
 def main():
     compute_views_for_bpi17()
-
-def get_ocel(filename, leading_type, object_types, act_name, time_name, sep):
-    parameters = {
-        "obj_names": object_types,
-        "val_names": [],
-        "act_name": act_name,
-        "time_name": time_name,
-        "sep": sep,
-        "execution_extraction": "leading_type",
-        "leading_type": leading_type
-    }
-    ocel = ocel_import_factory.apply(file_path=filename, parameters=parameters)
-    return ocel
-
-
-def compute_edges_by_leading_type(filename, object_types, act_name, time_name, sep):
-    edges_leading_types = []
-
-    for i, obj_type in enumerate(object_types):
-        ocel = get_ocel(filename, obj_type, object_types, act_name, time_name, sep)
-        relation = set()
-
-        for j, proc_exec in enumerate(ocel.process_executions):
-            proc_exec_graph = ocel.get_process_execution_graph(j)
-            relation.update(proc_exec_graph.edges)
-        edges_leading_types.append((i, relation))
-
-    return edges_leading_types
-
 
 def compute_views_for_bpi17():
     filename = '../../data/BPI2017-Final-adapt.csv'
