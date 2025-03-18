85c3e433748ce942ca8ab217110f38a3d80fa660
Maike Basmer <maike.basmer@hu-berlin.de>
2025-03-18 14:48:43 +0100

    added log filtering into eval

--- src/evaluation/ocel_contexts_eval.py
+++ src/evaluation/ocel_contexts_eval.py
@@ -142,18 +142,19 @@ def compute_views_for_bpi17_csv(k=None, weight=0.5, selection_method="mmr", duck
                   selection_method=selection_method, duckdb_config=duckdb_config, short_name="BPI17csv")
 
 
-def compute_views_for_bpi14(k=None, weight=0.5, selection_method="mmr", duckdb_config=None, filter_date="2013-09-30T23:59:59"):
+def compute_views_for_bpi14(k=None, weight=0.5, selection_method="mmr", duckdb_config=None, filter_date="2013-11-30T23:59:59"):
     # data = load_ocel_from_file("data/order-management.jsonocel")
     data = load_ocel_from_file("data/BPIC14.jsonocel.zip")
-    filtered_data = filter_ocel_json(data, start_time=("2013-01-01T00:00:01"), end_time=filter_date)
+    filtered_data = filter_ocel_json(data, start_time=("2013-11-01T00:00:01"), end_time=filter_date)
 
-    filename = f'data/bpi14-filtered-{filter_date.split("T")[0]}.jsonocel'
+    filename = f'data/bpi14-filtered-{filter_date.split("T")[0]}-Nov2013.jsonocel'
     with open(filename, "w") as f:
         f.write(json.dumps(filtered_data, indent=4))
 
     object_types = ["ConfigurationItem", "ServiceComponent", "Incident", "Interaction", "Change", "Case_R", "KM"]
     k = len(object_types) if k is None else k
-    db_file = db_path + f"leading_type_views_bpi14-filtered-{filter_date.split('T')[0]}.duckdb"
+   # db_file = db_path + f"leading_type_views_bpi14-filtered-{filter_date.split('T')[0]}.duckdb"
+    db_file = db_path + f"leading_type_views_bpi14-filtered.duckdb"
     assert k <= len(object_types), "k must be less than the number of object types"
     compute_views(filename, object_types, db_file, k=k, weight=weight, selection_method=selection_method,
                   duckdb_config=duckdb_config, short_name="BPI14")
